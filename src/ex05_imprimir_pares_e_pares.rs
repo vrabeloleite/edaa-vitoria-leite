1d7167a8f258467824c69cce50b10c643e2b9626
Vitória Rabelo Leite <vrabeloleite@gmail.com>
2026-06-01 20:26:47 -0300

    Update ex05_imprimir_pares_e_pares.rs

--- src/ex05_imprimir_pares_e_pares.rs
+++ src/ex05_imprimir_pares_e_pares.rs
@@ -1,7 +1,9 @@
 pub fn imprimir_pares_e_pares(lista: &[i32]) {
+    // bloco 1 printa sozinho
     for &x in lista {
         println!("{}", x);
     }
+
     for &x in lista {
         for &y in lista {
             println!("({}, {})", x, y);
@@ -14,7 +16,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_imprimir_existencia() {
+    fn test_imprimir() {
         imprimir_pares_e_pares(&[1, 2]);
         assert!(true);
     }
