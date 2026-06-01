a9b773d48665c18d94850fa6cd3a37a80d6c6c66
Vitória Rabelo Leite <vrabeloleite@gmail.com>
2026-06-01 20:21:22 -0300

    Create ex09_produto_de_matrizes.rs

---
+++ src/ex09_produto_de_matrizes.rs
@@ -0,0 +1,26 @@
+pub fn produto_de_matrizes(a: &[Vec<i64>], b: &[Vec<i64>]) -> Vec<Vec<i64>> {
+    let n = a.len();
+    let mut c = vec![vec![0i64; n]; n];
+
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..n {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    c
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_produto_matrizes() {
+        let a = vec![vec![1, 2], vec![3, 4]];
+        let b = vec![vec![2, 0], vec![1, 2]];
+        let resultado = produto_de_matrizes(&a, &b);
+        assert_eq!(resultado, vec![vec![4, 4], vec![10, 8]]);
+    }
+}
