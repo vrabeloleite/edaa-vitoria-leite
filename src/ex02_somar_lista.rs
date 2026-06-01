a7f72de7979458d49cfe580520dc3311f80af606
Vitória Rabelo Leite <vrabeloleite@gmail.com>
2026-06-01 20:17:40 -0300

    ex02_somar_lista.rs

---
+++ src/ex02_somar_lista.rs
@@ -0,0 +1,18 @@
+pub fn somar_lista(lista: &[i32]) -> i32 {
+    let mut total = 0;
+    for &elemento in lista {
+        total += elemento;
+    }
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_somar_lista() {
+        assert_eq!(somar_lista(&[1, 2, 3, 4, 5]), 15);
+        assert_eq!(somar_lista(&[]), 0);
+    }
+}
