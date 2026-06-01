446df38dcc0711d95842641d6591928286d65426
Vitória Rabelo Leite <vrabeloleite@gmail.com>
2026-06-01 20:13:48 -0300

    main.rs

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// Declaração dos módulos dos 10 exercícios
+
+pub mod src/ex01_verificar_primeiro.rs
+pub mod src/ex02_somar_lista.rs;
+pub mod src/ex03_busca_binaria.rs;
+pub mod src/ex04_pares_com_soma.rs;
+pub mod src/ex05_imprimir_pares_e_pares.rs;
+pub mod src/ex06_potencias_de_dois.rs;
+pub mod src/ex07_fibonacci_recursivo.rs;
+pub mod src/ex08_ordenacao_bolha.rs;
+pub mod src/ex09_produto_de_matrizes.rs;
+pub mod src/ex10_merge_sort.rs;
+
+fn main() {
+    println!("=== Atividade de EDAA: Algoritmos em Rust ===");
+    println!("Use o comando 'cargo test' para executar os testes automatizados!");
+}
