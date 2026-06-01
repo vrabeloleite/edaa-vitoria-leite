da77ecc3e3ec5266b046294e64a01d08f1a68a86
Vitória Rabelo Leite <vrabeloleite@gmail.com>
2026-06-01 20:29:03 -0300

    Update ex01_verificar_primeiro.rs

--- src/ex01_verificar_primeiro.rs
+++ src/ex01_verificar_primeiro.rs
@@ -13,6 +13,5 @@ mod tests {
     #[test]
     fn test_verificar_primeiro() {
         assert_eq!(verificar_primeiro(&[10, 20, 30]), Some(10));
-        assert_eq!(verificar_primeiro(&[]), None);
     }
 }
