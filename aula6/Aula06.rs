cf2038b950e2d97c9115d8a3a28f3a813fbb26cf
Vitória Rabelo Leite <vrabeloleite@gmail.com>
2026-06-11 22:01:01 -0300

    Tentativa erro demodulo

--- aula6/Aula06.rs
+++ aula6/Aula06.rs
@@ -3,3 +3,8 @@ pub mod grupo2_pilha;
 pub mod grupo3_fila;
 pub mod grupo4_deque;
 pub mod grupo5_extras;
+
+ fn main() {
+    println!("=== AULA 06 - TADS LINEARES ===");
+    println!("Compilação OK - exercícios na pasta aula6/");
+}
